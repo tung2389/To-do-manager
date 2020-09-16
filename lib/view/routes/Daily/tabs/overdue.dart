a5d530b33c064eedb6d0cbea7a03caa6781e794f
unknown <luukhanhtung2002@gmail.com>
2020-09-16 16:21:47 +0700

    Update overdue tab description

--- lib/view/routes/Daily/tabs/overdue.dart
+++ lib/view/routes/Daily/tabs/overdue.dart
@@ -9,52 +9,59 @@ class OverdueTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FutureBuilder<String>(
-        future:  LocalData.getUserId(),
-        builder: (context, AsyncSnapshot<String> uid) {
-          if(uid.hasData) {
-            return StreamBuilder(
-                stream: Firestore.instance
-                                .collection('user')
-                                .document(uid.data)
-                                .collection('yesterdayOverdue')
-                                .snapshots(),
-                builder: (context, snapshot) {
-                  if(!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator()
-                    );
-                  }
-                  else {
-                    dynamic taskList = snapshot.data.documents;
-                    return ListView.builder(
-                      padding: EdgeInsets.all(10.0),
-                      itemBuilder: (context, index) {
-                        Map<String, dynamic> rawTask = taskList[index].data;
-                        rawTask['id'] = taskList[index].documentID; //insert ID to Map task
-                        DailyTask task = DailyTask.fromMap(rawTask);
-                        return DailyTaskView(
-                          task: task,
-                          parentContext: context,
-                          mode: 'viewOverdue',
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          Text('In this tab you can see dailly tasks that you missed yesterday'),
+          FutureBuilder<String>(
+            future:  LocalData.getUserId(),
+            builder: (context, AsyncSnapshot<String> uid) {
+              if(uid.hasData) {
+                return StreamBuilder(
+                    stream: Firestore.instance
+                                    .collection('user')
+                                    .document(uid.data)
+                                    .collection('yesterdayOverdue')
+                                    .snapshots(),
+                    builder: (context, snapshot) {
+                      if(!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator()
                         );
-                      }, 
-                      itemCount: taskList.length,
-                    );
-                  }
-                },
-            );
-          }
-          else {
-            return Center(
-              child: SizedBox(
-                child: CircularProgressIndicator(),
-                width: 60,
-                height: 60,
-              ),
-            );
-          }
-        }
+                      }
+                      else {
+                        dynamic taskList = snapshot.data.documents;
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          padding: EdgeInsets.all(10.0),
+                          itemBuilder: (context, index) {
+                            Map<String, dynamic> rawTask = taskList[index].data;
+                            rawTask['id'] = taskList[index].documentID; //insert ID to Map task
+                            DailyTask task = DailyTask.fromMap(rawTask);
+                            return DailyTaskView(
+                              task: task,
+                              parentContext: context,
+                              mode: 'viewOverdue',
+                            );
+                          }, 
+                          itemCount: taskList.length,
+                        );
+                      }
+                    },
+                );
+              }
+              else {
+                return Center(
+                  child: SizedBox(
+                    child: CircularProgressIndicator(),
+                    width: 60,
+                    height: 60,
+                  ),
+                );
+              }
+            }
+          ),
+        ],
       )
     );
   }
